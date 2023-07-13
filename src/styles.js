eb0eb7170a0b2ab537726b80284e28cf68906970
RJTmini <redbas3@gmail.com>
2023-07-13 23:54:11 +0900

    #11.1 end

--- src/styles.js
+++ src/styles.js
@@ -2,18 +2,30 @@ import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
 export const lightTheme = {
-  fontColor: "#2c2c2c",
-  bgColor: "lightgray",
+  fontColor: "rgba(38, 38, 38, 1)",
+  bgColor: "white",
 };
 
 export const darkTheme = {
-  fontColor: "lightgray",
+  fontColor: "white",
   bgColor: "#2c2c2c",
 };
 
 export const GlobalStyles = createGlobalStyle`
   ${reset}
+  input {
+    all:unset;
+  }
+  * {
+    box-sizing:border-box;
+  }
   body {
     background-color: ${(props) => props.theme.bgColor};
+    font-size:14px;
+    font-family:'Open Sans', sans-serif;
+    color: ${(props) => props.theme.fontColor};
+  }
+  a {
+    text-decoration: none;
   }
 `;
