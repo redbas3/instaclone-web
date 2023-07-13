6cc659d10394504f7da4bc6cebf8f3520b2c8af1
RJTmini <redbas3@gmail.com>
2023-07-13 17:19:30 +0900

    #9.6 end

--- src/App.js
+++ src/App.js
@@ -2,23 +2,16 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
+import { useReactiveVar } from "@apollo/client";
+import { isLoggedInVar } from "./apollo";
 
 function App() {
-  const isLoggedIn = false;
+  const isLoggedIn = useReactiveVar(isLoggedInVar);
   return (
     <div>
       <Router>
         <Routes>
           <Route path="/" element={isLoggedIn ? <Home /> : <Login />} />
-          <Route
-            path="/nomad-coders"
-            element={
-              <>
-                <div>test</div>
-                {!isLoggedIn ? "Plz. login" : ""}
-              </>
-            }
-          />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
