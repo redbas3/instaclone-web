09173880d863d13caf56d861103e0eb17faed8ad
RJTmini <redbas3@gmail.com>
2023-07-14 23:55:51 +0900

    #11.9 end

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./screens/Home";
 import Login from "./screens/Login";
 import NotFound from "./screens/NotFound";
-import { useReactiveVar } from "@apollo/client";
-import { darkModeVar, isLoggedInVar } from "./apollo";
+import { ApolloProvider, useReactiveVar } from "@apollo/client";
+import { client, darkModeVar, isLoggedInVar } from "./apollo";
 import { ThemeProvider } from "styled-components";
 import { GlobalStyles, darkTheme, lightTheme } from "./styles";
 import SignUp from "./screens/SignUp";
@@ -14,23 +14,25 @@ function App() {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
   const darkMode = useReactiveVar(darkModeVar);
   return (
-    <HelmetProvider>
-      <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-        <GlobalStyles />
-        <Router>
-          <Routes>
-            <Route
-              path={routes.home}
-              element={isLoggedIn ? <Home /> : <Login />}
-            />
-            {!isLoggedIn ? (
-              <Route path={routes.signUp} element={<SignUp />} />
-            ) : null}
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Router>
-      </ThemeProvider>
-    </HelmetProvider>
+    <ApolloProvider client={client}>
+      <HelmetProvider>
+        <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+          <GlobalStyles />
+          <Router>
+            <Routes>
+              <Route
+                path={routes.home}
+                element={isLoggedIn ? <Home /> : <Login />}
+              />
+              {!isLoggedIn ? (
+                <Route path={routes.signUp} element={<SignUp />} />
+              ) : null}
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Router>
+        </ThemeProvider>
+      </HelmetProvider>
+    </ApolloProvider>
   );
 }
 
